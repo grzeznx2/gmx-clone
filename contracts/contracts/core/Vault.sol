7cbff0babed0ca53dfdb5f61df92bd37d820bc84
grzeznx2 <grzeznx2@gmail.com>
2022-12-04 19:45:00 +0100

    Add _validate and _onlyGov methods

--- contracts/contracts/core/Vault.sol
+++ contracts/contracts/core/Vault.sol
@@ -2,13 +2,28 @@
 pragma solidity ^0.8.9;
 
 contract Vault {
+
+    // error messages to be replaced later
+    uint256 public error1;
+
     mapping(uint256 => string) public errors;
     address public errorController;
+    address public gov;
 
-    constructor(){}
+    constructor(){
+        gov = msg.sender;
+    }
 
-    function setError(uint256 errorCode, string calldata error) external {
+    function setError(uint256 _errorCode, string calldata _error) external {
         require(msg.sender == errorController, "Vault: invalid errorController");
-        errors[errorCode] = error;
+        errors[_errorCode] = _error;
+    }
+
+    function _onlyGov() private view {
+        _validate(msg.sender == gov, error1);
+    }
+
+    function _validate(bool _condition, uint256 _errorCode) private view {
+        require(_condition, errors[_errorCode]);
     }
 }
